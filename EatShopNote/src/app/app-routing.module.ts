04bf886f3611cb4ea83ff8008b25be4de21209b3
anikit chaudhary <ankit.chaudhary@pelatro.com>
2024-01-09 23:45:27 +0530

    BAsic component created

---
+++ EatShopNote/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { NgModule } from "@angular/core";
+
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRooutingModule {}
--- EatShopNote/src/app/app.module.ts
+++ EatShopNote/src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./login/login.component";
+import { LogoutComponent } from "./logout/logout.component";
+import { SignupComponent } from "./signup/signup.component";
+import { FormsModule } from "@angular/forms";
+import { AppRooutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LogoutComponent,
+    SignupComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRooutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
